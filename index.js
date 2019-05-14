02d7f8c6c900b90189ab26e99f00f0b2c7a929cb
ElwinghL <clement.leboeuf@edu.univ-fcomte.fr>
2019-05-14 04:07:35 +0200

    Création de nouveaux personnages

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const PREFIX = "!";
 const CODE = "```\n";
-let lang = "fr";
+const CODEJSON = "```JSON\n";
 /*
  
    ______            _                                                 _   
@@ -30,11 +30,11 @@ dotenv.config();
 const http = require('http');
 
 http.createServer((req, res) => {
-res.writeHead(200, {
-    'Content-type': 'text/plain'
-});
-    res.write('Hey');
-    res.end();
+	res.writeHead(200, {
+		'Content-type': 'text/plain'
+	});
+	res.write('Hey');
+	res.end();
 }).listen(4000);
 /*
  
@@ -67,19 +67,23 @@ client.on('message', (msg) => {
 		if (msg.channel.name.includes("bot")) {
 			let trigger = command[0];
 			switch (true) {
-				case new RegExp("d","i").test(trigger):
+				case new RegExp("d", "i").test(trigger):
 					let dices = command;
 					dices.shift();
-					ret = cmdDice(dices);
+					ret += cmdDice(dices);
+					break;
+				case new RegExp("new", "i").test(trigger):
+					let character = command[1];
+					ret += cmdNew(msg.member.id, character);
 					break;
 				default:
-				case command[0].match(new RegExp("help","i")):
-					ret = cmdHelp();
+				case command[0].match(new RegExp("help", "i")):
+					ret += cmdHelp();
 					break;
 			};
 			msg.reply(ret);
 		} else {
-			msg.reply(ret + "Je ne peux pas parler dans ce channel. Celui ci doit inclure la mention **bot**.\n");
+			msg.reply(ret + "I can't speak in a channel that does not include **bot**.\n");
 		}
 	};
 });
@@ -102,6 +106,8 @@ function cmdHelp() {
 	let ret = "";
 	//Dices
 	ret += helpDice();
+	//New
+	ret += helpNew();
 	//Help
 	ret += helpHelp();
 	return ret;
@@ -115,22 +121,68 @@ function cmdDice(dices = []) {
 	let sum = 0;
 	let ret = CODE;
 	if (dices.lenght != 0) {
-		for (let i =0; i<dices.length;++i) {
+		for (let i = 0; i < dices.length; ++i) {
 			if (!(/^\d+$/.test(dices[i]))) {
 				return "\n" + dices[i] + errNan() + "\n" + helpDice();
 			} else {
-				let max = parseInt(dices[i],10);
-				let res = randomInt(1,max);
+				let max = parseInt(dices[i], 10);
+				let res = randomInt(1, max);
 				sum += res;
-				ret += "1d"+dices[i]+"("+res+")";
-				ret += (i == dices.length - 1 ? " = " + sum + "\n":" + ");
+				ret += "1d" + dices[i] + "(" + res + ")";
+				ret += (i == dices.length - 1 ? " = " + sum + "\n" : " + ");
 			};
 		};
 	} else {
-		let res = randomInt(1,100);
-		ret += "1d100("+res+") = "+res+"\n";
+		let res = randomInt(1, 100);
+		ret += "1d100(" + res + ") = " + res + "\n";
 	};
 	return ret + CODE;
+};
+/**
+ * Create a new character
+ * @param {String} userID 
+ * @param {Json} character 
+ */
+function cmdNew(userID, character) {
+	console.log(character);
+	let ret = "";
+	let file = new charFile(userID, ".JSON");
+	if (file.exist("./characters/")) {
+		return errAlreadyExist();
+	} else {
+		try {
+			let content = charCreate(character);
+			file.setContent(content);
+			file.save("./characters/");
+			ret += "Character successfully created";
+		} catch (error) {
+			console.log(error);
+			return errWrongJSONFormat();
+		};
+	};
+	return ret;
+};
+/**
+ * Retourne le JSON si le character est correct
+ * @param {Json} character 
+ */
+function charCreate(character) {
+	let ret = JSON.parse(character);
+	let def = new charFile("default", ".JSON");
+	def.openJSon("./characters/");
+	let content = def.content;
+	let check = ret.length != content.lenght;
+	Object.keys(content).forEach(function (key) {
+		if (!ret.hasOwnProperty(key)) {
+			check = true;
+		};
+	});
+	console.log(ret["stats"].sum());
+	if (check || ret["stats"].sum() != 200) {
+		throw "Wrong object format.";
+	} else {
+		return JSON.stringify(ret);
+	};
 };
 /*
  
@@ -150,24 +202,12 @@ function cmdDice(dices = []) {
 function helpDice() {
 	let ret = "";
 	let command = "!d X Y Z...\n";
-	switch (lang) {
-		case "fr":
-			ret += "Pour lancer les dés :\n";
-			ret += CODE;
-			ret += command;
-			ret += "Lance un dé X, un dé Y et un dé Z et retourne la somme.\n";
-			ret += "Si aucun dé n'est spécifié, lance un dé 100.\n\n";
-			ret += CODE;
-			break;
-		default: 
-			ret += "To throw the dices :\n";
-			ret += CODE;
-			ret += command;
-			ret += "Throw respectively a X,Y and Z dice and sum the result.\n";
-			ret += "If no dice is specified, throw a d100.\n";
-			ret += CODE;
-			break;
-	};
+	ret += "To throw the dices :\n";
+	ret += CODE;
+	ret += command;
+	ret += "Throw respectively a X,Y and Z dice and sum the result.\n";
+	ret += "If no dice is specified, throw a d100.\n";
+	ret += CODE;
 	return ret;
 };
 /**
@@ -176,22 +216,29 @@ function helpDice() {
 function helpHelp() {
 	let ret = "";
 	let command = "!help\n";
-	switch (lang) {
-		case "fr":
-			ret += "Pour obtenir de l'aide sur l'utilisation de ce bot :\n";
-			ret += CODE;
-			ret += command;
-			ret += "Affiche l'aide des fonctions voulues.\n"
-			ret += CODE;
-			break;
-		default: 
-			ret += "To get help :\n";
-			ret += CODE;
-			ret += command;
-			ret += "Display the help for the desired features.\n"
-			ret += CODE;
-			break;
-	};
+	ret += "To get help :\n";
+	ret += CODE;
+	ret += command;
+	ret += "Display the help for the desired features.\n"
+	ret += CODE;
+	return ret;
+};
+
+function helpNew() {
+	let ret = "";
+	let command = "!new {...}\n";
+	ret += "To create a character :\n";
+	ret += CODE;
+	ret += command;
+	ret += CODE;
+	ret += "And then replace {...} by something like :\n";
+	ret += CODEJSON;
+	let def = new charFile("default", ".JSON");
+	def.openJSon("./characters/");
+	let content = JSON.stringify(def.content);
+	ret += content;
+	ret += CODE;
+	ret += "The sum of all stats must be equal to 200. The order of the differents property is not important.\n";
 	return ret;
 };
 /*
@@ -207,28 +254,64 @@ function helpHelp() {
  
 */
 /**
- * Erreur : is not a number
+ * Error : is not a number
  */
 function errNan() {
-	switch (lang) {
-		case "fr":
-			return " n'est pas un nombre.";
-		default:
-			return " is not a number.";
-	};
+	return " is not a number.";
 };
+/**
+ * Error : This file already exists
+ */
+function errAlreadyExist() {
+	return "This player already has a characte on this Server.";
+};
+/**
+ * Error : This character is not valid
+ */
+function errWrongJSONFormat() {
+	return "This character is not valid.\n" + helpNew();
+};
+
+/*
+ 
+   ______               _   _                                             _   __                      _        _               
+  |  ____|             | | (_)                                           | | /_/                     | |      (_)              
+  | |__ ___  _ __   ___| |_ _  ___  _ __  ___    ___ ___  _ __ ___  _ __ | | ___ _ __ ___   ___ _ __ | |_ __ _ _ _ __ ___  ___ 
+  |  __/ _ \| '_ \ / __| __| |/ _ \| '_ \/ __|  / __/ _ \| '_ ` _ \| '_ \| |/ _ \ '_ ` _ \ / _ \ '_ \| __/ _` | | '__/ _ \/ __|
+  | | | (_) | | | | (__| |_| | (_) | | | \__ \ | (_| (_) | | | | | | |_) | |  __/ | | | | |  __/ | | | || (_| | | | |  __/\__ \
+  |_|  \___/|_| |_|\___|\__|_|\___/|_| |_|___/  \___\___/|_| |_| |_| .__/|_|\___|_| |_| |_|\___|_| |_|\__\__,_|_|_|  \___||___/
+                                                                   | |                                                         
+                                                                   |_|                                                         
+ 
+*/
 /**
  * Return a random number between min and max
  * @param {Number} min Included
  * @param {Number} max Included
  */
-function randomInt(min,max) {
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
 };
 /**
- * Return the last array element
+ * Sum all the value from an object
  */
-Array.prototype.last = function() {
-	return this[this.length - 1];
+Object.prototype.sum = function () {
+	let sum = 0;
+	Object.keys(this).forEach(function (key) {
+		sum += (this[key] > 0 ? this[key] : -this[key]);
+	}, this)
+	return sum;
 };
-
+/*
+ 
+   ______ _ _       _____           _                 
+  |  ____(_) |     / ____|         | |                
+  | |__   _| | ___| (___  _   _ ___| |_ ___ _ __ ___  
+  |  __| | | |/ _ \\___ \| | | / __| __/ _ \ '_ ` _ \ 
+  | |    | | |  __/____) | |_| \__ \ ||  __/ | | | | |
+  |_|    |_|_|\___|_____/ \__, |___/\__\___|_| |_| |_|
+                           __/ |                      
+                          |___/                       
+ 
+*/
+const charFile = require("./Scripts/filesystem.js").charFile;
